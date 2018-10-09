f0114265c0f7f3c69dd9efa9c8adca0816585364
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-10-09 11:58:23 +0200

    partition

--- partition.py
+++ partition.py
@@ -1,16 +1,38 @@
 import sys
 
-def main(order, input_files):
-    print(order, input_files)
+def partition(order, data):
+    for left, right in order:
+        if right in data:
+            print(left, right)
+            memory = []
+            while left in data:
+                data.remove(left)
+                memory.append(left)
+
+            index = data.index(right)
+            while memory:
+                data.insert(index, memory.pop())
 
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
+def parse_order(order_lines):
+    for line in order_lines:
+        left, op, right = line.split(' ')
+        if op == '<':
+            yield left, right
+        if op == '>':
+            yield right, left
+
+def main(args):
     order_file = args.pop(0)
     with open(order_file) as f:
-        order = [line.strip() for line in f if line.strip()]
+        raw_order = [line.strip() for line in f if line.strip()]
+        order = list(parse_order(raw_order))
     data = []
     for path in args:
         with open(path) as f:
             data.extend([line.strip() for line in f if line.strip()])
-    main(order, data)
+    partition(order, data)
+    print(data)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
